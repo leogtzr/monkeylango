3e7a74e8e5ecc86163cee1d12353e35459d3f29b
leogtzr <leogutierrezramirez@gmail.com>
2021-03-24 17:56:38 -0700

    add REPL

---
+++ cmd/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/leogtzr/monkeylango/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello, %s! This is the monkey programming language!\n", user.Username)
+	repl.Start(os.Stdin, os.Stdout)
+}
