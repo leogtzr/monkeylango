71fb8e0885b1dc2bbb94fee8e7ae5b1e385a257a
leogtzr <leogutierrezramirez@gmail.com>
2021-04-09 11:44:19 -0600

    improve the REPL

--- repl/repl.go
+++ repl/repl.go
@@ -6,7 +6,7 @@ import (
 	"io"
 
 	"github.com/leogtzr/monkeylango/lexer"
-	"github.com/leogtzr/monkeylango/token"
+	"github.com/leogtzr/monkeylango/parser"
 )
 
 const PROMPT = ">> "
@@ -22,11 +22,23 @@ func Start(in io.Reader, out io.Writer) {
 		}
 
 		line := scanner.Text()
-
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, fmt.Sprintf("\t%s\n", msg))
 	}
 }
