b5cb357c358c435ccbaff6bd0b43720b51579ddb
leogtzr <leogutierrezramirez@gmail.com>
2021-04-12 10:23:39 -0600

    add internal object types

---
+++ object/object.go
@@ -0,0 +1,45 @@
+package object
+
+import "fmt"
+
+type ObjectType string
+
+const (
+	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
+)
+
+type Object interface {
+	Type() string
+	Inspect() string
+}
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Inspect() string {
+	return fmt.Sprintf("%d", i.Value)
+}
+
+func (i *Integer) Type() string {
+	return INTEGER_OBJ
+}
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Type() string {
+	return BOOLEAN_OBJ
+}
+
+func (b *Boolean) Inspect() string {
+	return fmt.Sprintf("%t", b.Value)
+}
+
+type Null struct{}
+
+func (n *Null) Type() ObjectType { return NULL_OBJ }
+func (n *Null) Inspect() string  { return "null" }
