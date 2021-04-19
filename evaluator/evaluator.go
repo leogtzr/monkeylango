b8b74ce87ce4a1b3f823049a0ba27cd7ebab624e
leogtzr <leogutierrezramirez@gmail.com>
2021-04-18 20:38:16 -0600

    avoid creating a new bool internal object when the expression is true (or false)

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,18 @@ import (
 	"github.com/leogtzr/monkeylango/object"
 )
 
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
+}
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 
@@ -18,7 +30,8 @@ func Eval(node ast.Node) object.Object {
 		return &object.Integer{Value: node.Value}
 
 	case *ast.Boolean:
-		return &object.Boolean{Value: node.Value}
+		// return &object.Boolean{Value: node.Value}
+		return nativeBoolToBooleanObject(node.Value)
 	}
 
 	return nil
