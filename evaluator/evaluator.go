99b381869fd0327c541f851986c59ae3eda8b58f
leogtzr <leogutierrezramirez@gmail.com>
2021-04-19 18:51:54 -0600

    evaluate minus op

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -31,10 +31,21 @@ func evalBangOperatorExpression(right object.Object) object.Object {
 	}
 }
 
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -(value)}
+}
+
 func evalPrefixExpression(operator string, right object.Object) object.Object {
 	switch operator {
 	case "!":
 		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
 	default:
 		return NULL
 	}
