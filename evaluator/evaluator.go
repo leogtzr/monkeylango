e886854497d95f856d0fe96be68cf17bd0b9dd96
leogtzr <leogutierrezramirez@gmail.com>
2021-04-21 17:40:52 -0600

    add sponsors button

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -82,6 +82,12 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
 
+	case operator == "==":
+		return nativeBoolToBooleanObject(left == right)
+
+	case operator == "!=":
+		return nativeBoolToBooleanObject(left != right)
+
 	default:
 		return NULL
 	}
